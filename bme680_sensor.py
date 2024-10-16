e2b2b9254509af01479e9fa553d35f16286bfb69
Tommy Condon <114081733+tommyc2@users.noreply.github.com>
2024-10-16 11:54:00 +0100

    Create bme680_sensor.py

---
+++ bme680_sensor.py
@@ -0,0 +1,15 @@
+import random
+
+def get_temperature():
+    data = round((random.random()*100),2)
+    return data
+
+def get_humidity():
+    data = round((random.random()*100),2)
+    return data
+
+def get_pressure():
+    data = round((random.random()*100),2)
+    return data
+
+
